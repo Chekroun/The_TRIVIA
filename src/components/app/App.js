4cef2855a2e89c0219763aea705f7b55412cdc5a
Mickael Chekroun <mika@MacBook-Pro-de-Mickael.local>
2018-10-24 16:53:18 +0200

    Mise à jour des betises de mika

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Question from '../Questions'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Question/>
+        Coucou
       </div>
     );
   }
